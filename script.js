17fb63c5c2aa75cba5aa2a2a06840a9f2f138f57
Hari Patel <haripatel788@gmail.com>
2025-04-21 21:00:33 -0400

    built website

---
+++ script.js
@@ -0,0 +1,10 @@
+// Example of smooth scroll for anchors (if you have in-page links)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
