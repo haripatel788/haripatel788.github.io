baa877e47678a81d743c277461449116dd327965
Hari Patel <haripatel788@gmail.com>
2025-04-22 23:13:28 -0400

    FINAL UPDATE

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Example of smooth scroll for anchors (if you have in-page links)
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -10,7 +10,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
   
 
-  // Page load fade-in effect
+
 window.addEventListener('load', () => {
   const fadeSections = document.querySelectorAll('.fade-section');
   fadeSections.forEach((section) => {
@@ -18,7 +18,7 @@ window.addEventListener('load', () => {
   });
 });
 
-// Scroll-based fade-in/out effect
+
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
@@ -28,7 +28,7 @@ const observer = new IntersectionObserver((entries, observer) => {
     }
   });
 }, {
-  threshold: 0.5 // Trigger when 50% of the section is visible
+  threshold: 0.5
 });
 
 document.querySelectorAll('.fade-section').forEach(section => {
