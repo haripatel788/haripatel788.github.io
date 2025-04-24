06663eddb95bf20c2cd74cbab793fe52b4e0e8dd
Hari Patel <haripatel788@gmail.com>
2025-04-23 21:23:26 -0400

    mobile friendly changes

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
-
-  
-
-
-window.addEventListener('load', () => {
-  const fadeSections = document.querySelectorAll('.fade-section');
-  fadeSections.forEach((section) => {
-    section.classList.add('fade-in-visible');
-  });
 });
-
 
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
@@ -35,3 +24,21 @@ document.querySelectorAll('.fade-section').forEach(section => {
   observer.observe(section);
 });
 
+document.getElementById("menu-btn")?.addEventListener("click", () => {
+  const menu = document.getElementById("mobile-menu");
+  menu.classList.toggle("hidden");
+});
+
+document.querySelectorAll("#mobile-menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    const menu = document.getElementById("mobile-menu");
+    menu.classList.add("hidden");
+  });
+});
+
+window.addEventListener('load', () => {
+  const fadeSections = document.querySelectorAll('.fade-section');
+  fadeSections.forEach((section) => {
+    section.classList.add('fade-in-visible');
+  });
+});
